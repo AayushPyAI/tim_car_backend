307b58454c8fb1cd73f0b692c73aab0781b6e25a
Aayush-saini <aayush.excel2011@gmail.com>
2025-06-23 12:54:56 +0530

    dupoint fixes

--- scrapers/dupont/dupont.py
+++ scrapers/dupont/dupont.py
@@ -5,13 +5,18 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from urllib.parse import urljoin
 from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def get_dupont_listings():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("window-size=1920,1080")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -19,8 +24,12 @@ def get_dupont_listings():
 
     try:
         driver.get("https://www.dupontregistry.com/autos/results/all/all/private/filter:categories=exotic")
+        # Wait for the cards to load
+        wait = WebDriverWait(driver, 30)
+        cards = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.LilCards-module_product_wrapper__FkNih')))
+       
     except Exception as e:
-        print("Error loading page:", e)
+        print("Error loading page or finding cards:", e)
         driver.quit()
         return []
 
@@ -106,10 +115,10 @@ def get_dupont_listings():
     driver.quit()
     return listings
 
-# if __name__ == "__main__":
-#     data = get_dupont_listings()
-#     for d in data:
-#         print(d)
+if __name__ == "__main__":
+    data = get_dupont_listings()
+    for d in data:
+        print(d)
 
 
 
